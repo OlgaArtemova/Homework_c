9c31190bace1922a9e19de7b36c1b4534794696c
OlgaArtemova <61464430+OlgaArtemova@users.noreply.github.com>
2023-06-22 11:53:50 +0400

    Add task 2, 4, 6

--- Program.cs
+++ Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+/*
+Console.Write("Введите первое число: ");
+string number = Console.ReadLine()!;
+int num1 = int.Parse(number);
+Console.Write("Введите второе число: ");
+number = Console.ReadLine()!;
+int num2 = int.Parse(number);
+if (num1 > num2)
+{
+    Console.WriteLine($"Максимальное число: {num1}");
+}
+else
+{
+    Console.WriteLine($"Максимальное число: {num2}");
+}
+*/
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+/*
+Console.Write("Введите первое число: ");
+string number = Console.ReadLine()!;
+int num1 = int.Parse(number);
+Console.Write("Введите второе число: ");
+number = Console.ReadLine()!;
+int num2 = int.Parse(number);
+Console.Write("Введите третье число: ");
+number = Console.ReadLine()!;
+int num3 = int.Parse(number);
+if (num1 > num2)
+{
+    if (num3 > num1)
+    {
+        Console.WriteLine($"Максимальное число: {num3}");
+    }
+    else Console.WriteLine($"Максимальное число: {num1}");
+}
+else if (num2 > num3)
+{
+    Console.WriteLine($"Максимальное число: {num2}");
+}
+else
+{
+    Console.WriteLine($"Максимальное число: {num3}");
+}
+*/
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+/*
+Console.Write("Введите целое число: ");
+string number = Console.ReadLine()!;
+int num = int.Parse(number);
+Console.WriteLine(num%2 == 0);
+*/
