be1365b87907dafa773b6a7737d3da032820790f
OlgaArtemova <61464430+OlgaArtemova@users.noreply.github.com>
2023-07-02 10:05:07 +0400

    Task 21

--- Program.cs
+++ Program.cs
@@ -141,7 +141,7 @@ Console.WriteLine(result);
 */
 
 // Задача 19 : Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
-
+/*
 void CheckArray(int[] array)
 {
     for (int i = 0; i < array.Length; i++)
@@ -169,4 +169,27 @@ for (int i = 0; i < array.Length; i++)
     num /= 10;
 }
 CheckArray(array);
+*/
 
+//Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+int InputNum (string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+double Distance (int x1, int y1, int z1, int x2, int y2,  int z2 )
+{
+   double result = Math.Sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1) + (z2 - z1)*(z2 - z1));
+   return result;
+}
+int x1 = InputNum("Введите координату x1: ");
+int y1 = InputNum("Введите координату y1: ");
+int z1 = InputNum("Введите координату z1: ");
+int x2 = InputNum("Введите координату x2: ");
+int y2 = InputNum("Введите координату y2: ");
+int z2 = InputNum("Введите координату z2: ");
+
+
+double res = Distance (x1, y1, z1, x2, y2, z2);
+System.Console.WriteLine(Math.Round(res, 2));
