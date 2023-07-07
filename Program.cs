3f9fce2ea94c84bfffcdfedf2a4288d81998f50b
OlgaArtemova <61464430+OlgaArtemova@users.noreply.github.com>
2023-07-07 10:37:49 +0400

    Task 36

--- Program.cs
+++ Program.cs
@@ -297,7 +297,7 @@ int finish = InputNum("Введите конечное значение диап
 int [] res = ArrayRandom(quantity, start, finish);
 PrintArray(res);
 */
-
+/*
 //Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
 //Напишите программу, которая покажет количество чётных чисел в массиве.
 //[345, 897, 568, 234] -> 2
@@ -350,3 +350,60 @@ string txt = PrintArray(myArray);
 Console.WriteLine(txt);
 int quantity = FindQuantityNum(myArray);
 Console.WriteLine($"Количество четных элементов равно: {quantity}");
+*/
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// //Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int InputNum(string message)
+{
+    System.Console.WriteLine(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int[] CreateArray(int size)
+{
+    return new int[size];
+}
+
+void FillArray(int[] array, int min, int max)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+}
+
+string PrintArray(int[] array)
+{
+    string res = String.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        res += array[i] + " ";
+    }
+    return res;
+}
+
+int SumNumOddPos(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        sum += array[i];
+    }
+    return sum;
+}
+
+int size = InputNum("Введите размер массива: ");
+int minValue = InputNum("Введите минимальное значение элемента: ");
+int maxValue = InputNum("Введите максимальное значение элемента: ");
+
+int[] myArray = CreateArray(size);
+FillArray(myArray, minValue, maxValue);
+string txt = PrintArray(myArray);
+System.Console.WriteLine(txt);
+int summa = SumNumOddPos(myArray);
+System.Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях равна: {summa}");
