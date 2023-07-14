484a6579e3b9dde0b362ba12022e9dd0bce800ed
OlgaArtemova <61464430+OlgaArtemova@users.noreply.github.com>
2023-07-14 23:54:01 +0400

    Task 54

--- Program.cs
+++ Program.cs
@@ -747,7 +747,7 @@ FindSerialNum(myArray, serial);
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
-
+/*
 int InputNum(string message)
 {
     Console.Write(message);
@@ -799,3 +799,73 @@ Fill2DArray(myArray, minValue, maxValue);
 Print2DArray(myArray);
 Console.WriteLine();
 ArithmeticMeanColumns(myArray);
+*/
+
+// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int[,] Create2DArray(int rows, int cols)
+{
+    return new int[rows, cols];
+}
+void Fill2DArray(int[,] array, int min, int max)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = rnd.Next(min, max + 1);
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]}\t");
+        Console.WriteLine();
+    }
+}
+
+void SelectionSort(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int col = 0; col < array.GetLength(1) - 1 - j; col++)
+            {
+                if (array[i, col] > array[i, col + 1])
+                {
+                    int temp = array[i, col];
+                    array[i, col] = array[i, col + 1];
+                    array[i, col + 1] = temp;
+                }
+            }
+        }
+    }
+}
+
+int rows = InputNum("Введите количество строк: ");
+int columns = InputNum("Введите количество столбцов: ");
+int minValue = InputNum("Введите минимальное значение диапазона: ");
+int maxValue = InputNum("Введите максимальное значение диапазона: ");
+
+int[,] myArray = Create2DArray(rows, columns);
+Fill2DArray(myArray, minValue, maxValue);
+Print2DArray(myArray);
+Console.WriteLine();
+SelectionSort(myArray);
+Print2DArray(myArray);
