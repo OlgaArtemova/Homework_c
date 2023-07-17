0db25b59308b4a7dc7b2bd8858f9dc49dd0641ec
OlgaArtemova <61464430+OlgaArtemova@users.noreply.github.com>
2023-07-17 19:51:42 +0400

    Task 60

--- Program.cs
+++ Program.cs
@@ -959,7 +959,7 @@ Console.WriteLine($"Наименьшая сумма элементов в стр
 // Результирующая матрица будет:
 // 18 20
 // 15 18
-
+/*
 int InputNum(string message)
 {
     Console.Write(message);
@@ -1029,3 +1029,76 @@ Fill2DArray(myArraySecond, minValue, maxValue);
 Print2DArray(myArraySecond);
 Console.WriteLine();
 MultiplicationOfMatrix(myArrayFirst, myArraySecond);
+*/
+
+// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел.
+//Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int[,,] Create3DArray(int rows, int cols, int layer)
+{
+    return new int[rows, cols, layer];
+}
+int[] CreateArray(int rows, int cols, int layer)
+{
+    return new int[rows * cols * layer];
+}
+
+void Fill3DArray(int[,,] array3D, int[] array)
+{
+    if (array3D.Length <= 90)
+    {
+        Random rnd = new Random();
+        array[0] = rnd.Next(10, 100);
+        for (int i = 1; i < array.Length; i++)
+        {
+            int number = rnd.Next(10, 100);
+            for (int j = 0; j < i; j++)
+            {
+                if (number == array[j])
+                {
+                    number = rnd.Next(10, 100);
+                    j = 0;
+                }
+            }
+            array[i] = number;
+        }
+        int num = 0;
+        for (int k = 0; k < array3D.GetLength(2); k++)
+        {
+            for (int i = 0; i < array3D.GetLength(0); i++)
+            {
+                for (int j = 0; j < array3D.GetLength(1); j++)
+                {
+                    array3D[i, j, k] = array[num];
+                    Console.Write($"{array3D[i, j, k]} ({i}, {j}, {k})\t");
+                    num++;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+    else
+    {
+        Console.WriteLine("Невозможно заполнить массив неповторяющимися двузначными числами");
+    }
+}
+
+int rows = InputNum("Введите количество строк: ");
+int columns = InputNum("Введите количество столбцов: ");
+int layers = InputNum("Введите количество слоев: ");
+
+int[,,] my3DArray = Create3DArray(rows, columns, layers);
+int[] myArray = CreateArray(rows, columns, layers);
+Fill3DArray(my3DArray, myArray);
+
