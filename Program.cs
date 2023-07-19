b4840f9f21dfeaa24647a2606900bafcaf3949ad
OlgaArtemova <61464430+OlgaArtemova@users.noreply.github.com>
2023-07-19 11:12:13 +0400

    task 66

--- Program.cs
+++ Program.cs
@@ -1153,7 +1153,7 @@ PrintArray(matrix);
 // Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
-
+/*
 int InputNum(string message)
 {
     Console.Write(message);
@@ -1168,3 +1168,23 @@ string PrintNum(int n)
 
 int num = InputNum("Введите целое число: ");
 System.Console.Write(PrintNum(num));
+*/
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+int SumNumbers(int start, int end)
+{
+    if (start == end) return end;
+    return (start + SumNumbers(++start, end));
+}
+
+int M = InputNum("Введите целое число M: ");
+int N = InputNum("Введите целое число N: ");
+System.Console.Write(SumNumbers(M, N));
